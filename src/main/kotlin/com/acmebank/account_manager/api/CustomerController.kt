55afe5a10b898e351c4c4c0f25c08a7729d3865b
Mariusz Szulc <Mariusz.Hubert.Szulc@gmail.com>
2024-07-10 14:51:12 +0800

    implement the balance endpoint

--- src/main/kotlin/com/acmebank/account_manager/api/CustomerController.kt
+++ src/main/kotlin/com/acmebank/account_manager/api/CustomerController.kt
@@ -1,15 +1,27 @@
 package com.acmebank.account_manager.api
 
 import com.acmebank.account_manager.api.dto.TransferRequestDto
+import com.acmebank.account_manager.persistence.CustomersRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/app")
-class CustomerController {
+class CustomerController(
+    @Autowired
+    val customersRepository: CustomersRepository
+) {
 
     @GetMapping("customer/{id}/balance")
     fun getCustomerBalance(@PathVariable("id") customerId: Int): String {
-        return customerId.toString()
+        // We don't need to validate {customerId}, spring does that automatically
+
+        val customer = customersRepository.findById(customerId)
+            ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "Customer $customerId not found")
+
+        return customer.balance.toPlainString()
     }
 
     @PostMapping("customer/{id}/transfer")
---
+++ src/main/kotlin/com/acmebank/account_manager/persistence/CustomersRepository.kt
@@ -0,0 +1,18 @@
+package com.acmebank.account_manager.persistence
+
+import jakarta.persistence.*
+import org.springframework.data.repository.Repository
+import java.math.BigDecimal
+
+@Entity
+@Table(name = "customers")
+class Customer(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    internal val id: Int,
+    internal val balance: BigDecimal
+)
+
+interface CustomersRepository : Repository<Customer, Int> {
+    fun findById(id: Int): Customer?
+}
